ce1e22db88d216406517ae933dbe04193bb7c790
preethampatnaik <preetham_2971993@yahoo.co.in>
2015-08-27 19:42:27 +0530

    Create Comparator.java

---
+++ Comparator.java
@@ -0,0 +1,24 @@
+package com.test1;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Vector;
+import java.util.Iterator;//if iterator is used
+
+
+public abstract class comparator implements Comparator{
+	public static void main(String[] args){
+		Vector<Integer> v = new Vector<Integer>(10);
+		v.add(30);
+		v.add(10);
+		v.add(20);
+		System.out.println(v);
+		Comparator<Integer> c = Collections.reverseOrder();/*returns a reference to C which when send as parameter to sort() will                                                           sort the unsorted Vector in descending order*/
+		Comparator a = Collections.reverseOrder(c);/*reverses the order of the Comparator hence return ref which when fed to sort()                                                 gives ascending order*/
+		Collections.sort(v,c);
+		System.out.println(v);
+		Collections.sort(v,a);
+		System.out.println(v);
+		
+	}
+
+}
